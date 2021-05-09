894d87fcd65e8834d24075b2736925d123464ebf
protolambda <proto@protolambda.com>
2021-05-09 07:53:52 +0200

    mount build script

--- docker_hack.sh
+++ docker_hack.sh
@@ -8,4 +8,4 @@ docker build -t prysmbuilder -f builder.Dockerfile .
 # - the docker socket to insert the produced images into the host docker environment
 # - the prysm dir, to cache the build, and don't have to copy all files over into the image for building.
 PRYSM_DIR=$(readlink -f ../prysm)
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PRYSM_DIR:/prysmbuilder" -w /prysmbuilder prysmbuilder ./docker_build.sh
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PRYSM_DIR:/prysmbuilder" -v "${PWD}/docker_build.sh:/docker_build.sh" -w /prysmbuilder prysmbuilder /docker_build.sh
